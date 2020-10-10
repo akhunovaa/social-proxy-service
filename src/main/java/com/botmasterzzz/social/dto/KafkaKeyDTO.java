cfe43edd198f56713b79f37746d83c3a660ba2af
Azat Akhunov <azat.akhunov@protonmail.com>
2020-10-10 17:44:13 +0300

    [NF] bot core updated

--- src/main/java/com/botmasterzzz/social/dto/KafkaKeyDTO.java
+++ src/main/java/com/botmasterzzz/social/dto/KafkaKeyDTO.java
@@ -8,9 +8,6 @@ public class KafkaKeyDTO implements Serializable {
 
     private Long instanceKey;
     private Integer updateId;
-    private String fileName;
-    private boolean loading;
-    private String userName;
 
     public Long getInstanceKey() {
         return instanceKey;
@@ -28,55 +25,17 @@ public class KafkaKeyDTO implements Serializable {
         this.updateId = updateId;
     }
 
-    public String getFileName() {
-        return fileName;
-    }
-
-    public void setFileName(String fileName) {
-        this.fileName = fileName;
-    }
-
-    public boolean isLoading() {
-        return loading;
-    }
-
-    public void setLoading(boolean loading) {
-        this.loading = loading;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    @Override
-    public String toString() {
-        return "KafkaKeyDTO{" +
-                "instanceKey=" + instanceKey +
-                ", updateId=" + updateId +
-                ", fileName='" + fileName + '\'' +
-                ", loading=" + loading +
-                ", userName='" + userName + '\'' +
-                '}';
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         KafkaKeyDTO that = (KafkaKeyDTO) o;
-        return loading == that.loading &&
-                Objects.equal(instanceKey, that.instanceKey) &&
-                Objects.equal(updateId, that.updateId) &&
-                Objects.equal(fileName, that.fileName) &&
-                Objects.equal(userName, that.userName);
+        return Objects.equal(instanceKey, that.instanceKey) &&
+                Objects.equal(updateId, that.updateId);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hashCode(instanceKey, updateId, fileName, loading, userName);
+        return Objects.hashCode(instanceKey, updateId);
     }
 }
